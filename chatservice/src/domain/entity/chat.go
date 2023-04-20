754009ecc7ee66bc323ece2b1344bffcf7f434d7
ThyagoFRTS <kzkr.thyago@gmail.com>
2023-04-20 10:13:19 -0300

    feat: create openai conversation

---
+++ chatservice/src/domain/entity/chat.go
@@ -0,0 +1,92 @@
+package entity
+
+import (
+	"errors"
+
+	"github.com/google/uuid"
+)
+
+type ChatConfig struct {
+	Model            *Model
+	Temperature      float32
+	TopP             float32
+	N                int
+	Stop             []string
+	MaxTokens        int
+	PresencePenalty  float32
+	FrequencyPenalty float32
+}
+
+type Chat struct {
+	ID                   string
+	UserID               string
+	InitialSystemMessage *Message
+	Messages             []*Message
+	ErasedMessages       []*Message
+	Status               string
+	TokenUsage           int
+	Config               *ChatConfig
+}
+
+func NewChat(userID string, initialSystemMessage *Message, chatConfig *ChatConfig) (*Chat, error) {
+	chat := &Chat{
+		ID:                   uuid.New().String(),
+		UserID:               userID,
+		InitialSystemMessage: initialSystemMessage,
+		Status:               "active",
+		Config:               chatConfig,
+		TokenUsage:           0,
+	}
+	chat.AddMessage(initialSystemMessage)
+
+	if err := chat.Validate(); err != nil {
+		return nil, err
+	}
+	return chat, nil
+}
+
+func (c *Chat) Validate() error {
+	if c.UserID == "" {
+		return errors.New("user id is empty")
+	}
+	if c.Status != "active" && c.Status != "ended" {
+		return errors.New("invalid status")
+	}
+	return nil
+}
+
+func (c *Chat) AddMessage(m *Message) error {
+	if c.Status == "ended" {
+		return errors.New("chat is ended")
+	}
+	for {
+		if c.Config.Model.GetMaxTokens() >= m.getAmountTokens()+c.TokenUsage {
+			c.Messages = append(c.Messages, m)
+		}
+		c.ErasedMessages = append(c.ErasedMessages, c.Messages[0])
+		c.Messages = c.Messages[1:]
+		c.RefreshTokenUsage()
+	}
+	return nil
+}
+
+func (c *Chat) RefreshTokenUsage() {
+	c.TokenUsage = 0
+	for m := range c.Messages {
+		c.TokenUsage += c.Messages[m].getAmountTokens()
+		c.RefreshTokenUsage()
+		break
+	}
+}
+
+func (c *Chat) GetMessages() []*Message {
+	return c.Messages
+}
+
+func (c *Chat) CountMessages() int {
+	return len(c.Messages)
+}
+
+func (c *Chat) End() {
+	c.Status = "ended"
+}
