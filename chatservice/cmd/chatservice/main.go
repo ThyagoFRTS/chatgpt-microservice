4f5654c16febb25b81481d61195e998638accf80
ThyagoFRTS <kzkr.thyago@gmail.com>
2023-04-23 12:34:22 -0300

    feat: create server functions

---
+++ chatservice/cmd/chatservice/main.go
@@ -0,0 +1,68 @@
+package chatservice
+
+import (
+	"database/sql"
+	"fmt"
+
+	"github.com/ThyagoFRTS/chatgpt-microservice/chatservice/configs"
+	"github.com/ThyagoFRTS/chatgpt-microservice/chatservice/internal/infra/repository"
+	"github.com/ThyagoFRTS/chatgpt-microservice/chatservice/internal/infra/web"
+	"github.com/ThyagoFRTS/chatgpt-microservice/chatservice/internal/infra/web/webserver"
+	"github.com/ThyagoFRTS/chatgpt-microservice/chatservice/internal/usecase/chatcompletion"
+
+	//"github.com/ThyagoFRTS/chatgpt-microservice/chatservice/internal/usecase/chatcompletionstream"
+	"github.com/sashabaranov/go-openai"
+)
+
+func main() {
+	configs, err := configs.LoadConfig(".")
+	if err != nil {
+		panic(err)
+	}
+
+	conn, err := sql.Open(configs.DBDriver, fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?parseTime=true&multiStatements=true",
+		configs.DBUser, configs.DBPassword, configs.DBHost, configs.DBPort, configs.DBName))
+	if err != nil {
+		panic(err)
+	}
+
+	defer conn.Close()
+
+	repo := repository.NewChatRepositoryMySQL(conn)
+	client := openai.NewClient(configs.OpenAIApiKey)
+
+	chatConfig := chatcompletion.ChatCompletionConfigInputDTO{
+		Model:                configs.Model,
+		ModelMaxTokens:       configs.ModelMaxTokens,
+		Temperature:          float32(configs.Temperature),
+		TopP:                 float32(configs.TopP),
+		N:                    configs.N,
+		Stop:                 configs.Stop,
+		MaxTokens:            configs.MaxTokens,
+		InitialSystemMessage: configs.InitialChatMessage,
+	}
+	/*
+		chatConfigStream := chatcompletionstream.ChatCompletionConfigInputDTO{
+			Model:                configs.Model,
+			ModelMaxTokens:       configs.ModelMaxTokens,
+			Temperature:          float32(configs.Temperature),
+			TopP:                 float32(configs.TopP),
+			N:                    configs.N,
+			Stop:                 configs.Stop,
+			MaxTokens:            configs.MaxTokens,
+			InitialSystemMessage: configs.InitialChatMessage,
+		}*/
+
+	usecase := chatcompletion.NewChatCompletionUseCase(repo, client)
+
+	//streamChannel := make(chan chatcompletionstream.ChatCompletionOutputDTO)
+	//usecaseStream := chatcompletionstream.NewChatCompletionUseCase(repo, client, streamChannel)
+
+	webserver := webserver.NewWebServer(":" + configs.WebServerPort)
+
+	webserverChatHandler := web.NewWebChatGPTHandler(*usecase, chatConfig, configs.AuthToken)
+
+	webserver.AddHandler("/chat", webserverChatHandler.Handle)
+	fmt.Println("Starting server on port " + configs.WebServerPort)
+
+}
